a8d149bf10e9913c34a317c8ab4d461f65c4966c
Anubhav Gautam <139226792+AgKing3242@users.noreply.github.com>
2024-05-15 11:09:53 +0530

    Add files via upload

--- DFU firmware upload/dfu.sh
+++ DFU firmware upload/dfu.sh
@@ -1,16 +1,25 @@
-key=$1
-file_name=$2
-if [ "$key" == "" ]; then
+key1=$1
+key2=$2
+key3=$3
+int_value=$(sudo dfu-util -l | grep "Internal" | cut -d " " -f 7 | tr -d "intf=,")
+alt_value=$(sudo dfu-util -l | grep "Internal" | cut -d " " -f 9 | tr -d "alt=,")
+if [ "$key1" == "" ]; then
 	echo "Please specify a key."
-elif [ "$key" == "list" ]; then
+elif [ "$key1" == "list" ]; then
 	sudo dfu-util -l
-elif [ "$key" == "upload" ]; then
-	if [ "$2" == "" ]; then
+elif [ "$key1" == "upload" ]; then
+	if [ "$key2" == "" ]; then
 		echo "Please specify the name of the file."
+	elif [ "$key2" == "-p" ]; then
+		if [ "$key3" == "" ]; then
+			echo "Please specify the path to the file."
+		else
+			sudo dfu-util -a "$alt_value" -i "$int_value" -s 0x08000000:leave -D $key3
+		fi
 	else
-		int_value=$(sudo dfu-util -l | grep "Internal" | cut -d " " -f 7 | tr -d "intf=,")
-		alt_value=$(sudo dfu-util -l | grep "Internal" | cut -d " " -f 9 | tr -d "alt=,")
-		sudo dfu-util -a "$alt_value" -i "$int_value" -s 0x08000000:leave -D ~/Arduino/"$file_name"/build/"STMicroelectronics.stm32.GenF4"/"$file_name".ino.bin
+		sudo dfu-util -a "$alt_value" -i "$int_value" -s 0x08000000:leave -D ~/Arduino/"$key2"/build/"STMicroelectronics.stm32.GenF4"/"$key2".ino.bin
 		echo "Firmware flashed succesfully"
 	fi
+else
+	echo "Invalid command."
 fi
